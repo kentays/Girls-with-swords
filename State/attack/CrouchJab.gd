ae4b8bac1e4e94a5fc5c367da490f532fdf2bc48
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-07-01 10:08:48 -0400

    Fixed sweep

--- State/attack/CrouchJab.gd
+++ State/attack/CrouchJab.gd
@@ -2,9 +2,12 @@ extends "BaseAttack.gd"
 			
 			
 func handle_input(event: InputEvent):
-	if hit_connect and event.is_action_pressed(input_dict["jump"]):
-		print("jump cancel")
-		emit_signal("finished", "jump")
+	if hit_connect: 
+		if event.is_action_pressed(input_dict["jump"]):
+			print("jump cancel")
+			emit_signal("finished", "jump")
+		elif event.is_action_pressed(input_dict["kick"]):
+			emit_signal("finished", "sweep")
 	
 func _on_animation_finished():
 	if Input.is_action_pressed(input_dict["crouch"]):
--- State/attack/Sweep.gd
+++ State/attack/Sweep.gd
@@ -2,6 +2,4 @@ extends "CrouchJab.gd"
 
 # needs better hitbox and timing
 
-func enter():
-	owner.whiff()
 			
