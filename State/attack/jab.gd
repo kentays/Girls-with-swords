33bf6eabfc166d6246734c33b0d684016e378ae2
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-07-10 13:30:09 -0400

    gatlings

--- State/attack/jab.gd
+++ State/attack/jab.gd
@@ -1,10 +1,16 @@
 extends "BaseAttack.gd"
 			
 func handle_input(event: InputEvent):
-	if hit_connect and event.is_action_pressed(input_dict["jump"]):
-		emit_signal("finished", "jump")
-	elif hit_connect and event.is_action_pressed(input_dict["slash"]):
-		emit_signal("finished", "slash")	
+	if hit_connect:
+		print("Attempting to gatling...")
+		if event.is_action_pressed(input_dict["jump"]):
+			emit_signal("finished", "jump")
+		elif event.is_action_pressed(input_dict["kick"]):
+			print("Gatling to kick")
+			emit_signal("finished", "kick")	
+		elif event.is_action_pressed(input_dict["slash"]):
+			print("Gatling to slash")
+			emit_signal("finished", "slash")	
 		
 func _on_animation_finished():
 	emit_signal("finished", "idle")
