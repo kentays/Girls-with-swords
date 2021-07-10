6b3bcf35c36a552921ae6d7a7852433e8886ed3c
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-07-10 16:55:40 -0400

    CrouchJab gatlings

--- State/attack/CrouchJab.gd
+++ State/attack/CrouchJab.gd
@@ -1,13 +1,9 @@
 extends "BaseAttack.gd"
 			
 			
-func handle_input(event: InputEvent):
-	if hit_connect: 
-		if event.is_action_pressed(input_dict["jump"]):
-			print("jump cancel")
-			emit_signal("finished", "jump")
-		elif event.is_action_pressed(input_dict["kick"]):
-			emit_signal("finished", "sweep")
+func _init():
+	add_gatling("kick", "sweep")
+	add_gatling("slash", "crouch_slash")
 	
 func _on_animation_finished():
 	if Input.is_action_pressed(input_dict["crouch"]):
