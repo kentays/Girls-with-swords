de0a9fa6f16bb9bfb6c4ed5ee9847dee2281de33
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-07-11 19:59:45 -0400

    Fixed hadouken

--- State/move/run.gd
+++ State/move/run.gd
@@ -36,12 +36,9 @@ func handle_input(event: InputEvent):
 		emit_signal("finished", "kick")
 		
 	elif event.is_action_pressed(input_dict["slash"]):
-		if len(owner.input_buf) > 2:
-			var dir: String = "move_right"
-			if not owner.facing_right: dir = "move_left"
-			if (owner.input_buf[1] == dir) and (owner.input_buf[2] == "crouch"):
-				owner.stop_sfx()
-				emit_signal("finished", "hadouken")
+		if owner.check_complex_buffer(["crouch", "move_right", "stand"]):
+			owner.stop_sfx()
+			emit_signal("finished", "hadouken")
 
 		else:
 			emit_signal("finished", "slash")
